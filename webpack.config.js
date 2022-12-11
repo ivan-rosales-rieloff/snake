fe347e6dde7bd0f460d74592e84924c4fd83b707
Ivan Rieloff <ivan.rosales.rieloff@gmail.com>
2022-12-11 16:18:16 -0300

    angular cli - missing files

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-module.exports = {
-  entry: "./site/index.js",
-  output: {
-    path: path.resolve(__dirname + 'site', "dist"),
-    filename: "index.js",
-  },
-  mode: "development",
-};
